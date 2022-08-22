af024f95b86e1b9cbb6f2968b7dd2e1b121c115d
Eljaduen <eljaduen@uninorte.edu.co>
2022-08-22 09:02:25 -0500

    Version final

--- JS.js
+++ JS.js
@@ -1,7 +1,66 @@
-// const myModal = new bootstrap.Modal(document.getElementById('Modal'), options)
-const myModal = document.getElementById('mimodal')
-const myInput = document.getElementById('modal')
+const sginput = document.querySelectorAll('.form-contorno')
+const formula = document.getElementById('formulario-modal')
+const confirma = document.querySelectorAll('.texto')
+const usuario = `<small class="form-error">Ups, algo pasó...</small>`
+const errorcheck = `<small class="form-error">Ups, el check...</small>`
+const email = document.getElementById('exampleInputEmail1modal')
+const mirar = document.getElementById('check-modal').checked
+function isEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
 
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
+  confirma.forEach((texto, indice) => {
+    formula.addEventListener("submit", function(e){
+        e.preventDefault();
+        revisarTexto(indice) 
+        addErrores(indice)
+    }) 
+         
 })
+
+    
+        
+function revisarTexto (indice) {
+  if (confirma[indice].value === '') {
+    confirma[indice].classList.add("error");
+  } else if (indice == 1) {
+    if (!isEmail(email)) {
+      confirma[indice].classList.add("error");
+    }
+  }else {
+    confirma[indice].classList.remove("error")
+  }
+   
+    
+}
+function addErrores(indice){
+  if (confirma[indice].value === '') {
+    sginput[indice].innerHTML =  '';
+    sginput[indice].innerHTML +=  usuario;
+  } else if (indice == 1) {
+    if (!isEmail(email)) {
+      sginput[indice].innerHTML =  '';
+      sginput[indice].innerHTML +=  usuario;
+      
+    }
+  } else if (indice == 2) {
+    if (mirar === false){ 
+      sginput[indice].innerHTML =  '';
+      sginput[indice].innerHTML +=  errorcheck;
+    }
+  }
+}
+
+  
+  
+
+
+document.querySelectorAll(".texto").forEach((element, indice) => {
+    element.addEventListener("focus", (event) => {
+      console.log(event);
+      event.currentTarget.classList.remove("error")
+      sginput[indice].innerHTML = '';
+      console.log(mirar)
+  })
+})
+
